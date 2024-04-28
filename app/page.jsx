532ced2d68aa2b882c7d59c8cd40b7ee09e59070
tapadar13 <monsurismyname@gmail.com>
2024-04-28 09:57:28 +0530

    feat: Implement InfoBoxes component to provide concise information and call-to-action for renters and property owners, enhancing user engagement and navigation on the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
 import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+  );
 }
